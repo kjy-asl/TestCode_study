a4a5d12f9179047d248180071f0c572b67dc0403
김주영 <gimjuyeong0620@gmail.com>
2024-01-18 19:20:21 +0900

    refactor

--- src/main/java/com/example/sample/SampleApplication.java
+++ src/main/java/com/example/sample/SampleApplication.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class SampleApplication {
 
     public static void main(String[] args){
-        String[] parts = new CalculationRequestReader().read();
+        CalculationRequest calculationRequest = new CalculationRequestReader().read();
+        long answer = new Calculator().calculate(
+                calculationRequest.getNum1(),
+                calculationRequest.getOperator(),
+                calculationRequest.getNum2());
 
-        long num1 = Long.parseLong(parts[0]);
-        long num2 = Long.parseLong(parts[2]);
-        String operator = parts[1];
-        long answer = new Calculator().calculate(num1, operator, num2);
         System.out.println(answer);
     }
 }
