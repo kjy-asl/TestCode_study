fb3c9b345f6ea93c461a4b6081fad8cd74699742
김주영 <gimjuyeong0620@gmail.com>
2024-01-18 19:07:58 +0900

    refactor : input function refactoring

--- src/main/java/com/example/sample/SampleApplication.java
+++ src/main/java/com/example/sample/SampleApplication.java
@@ -5,17 +5,12 @@ import java.util.Scanner;
 public class SampleApplication {
 
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter two numbers and an operator (e.g 1 + 2): ");
-        String result = scanner.nextLine();
-        String[] parts = result.split(" ");
-
-        Calculator calculator = new Calculator();
+        String[] parts = new CalculationRequestReader().read();
 
         long num1 = Long.parseLong(parts[0]);
         long num2 = Long.parseLong(parts[2]);
         String operator = parts[1];
-        long answer = calculator.calculate(num1, operator, num2);
+        long answer = new Calculator().calculate(num1, operator, num2);
         System.out.println(answer);
     }
 }
