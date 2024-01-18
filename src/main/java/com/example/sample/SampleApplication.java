dd4308f6360605d273c7183c760856544c16c933
김주영 <gimjuyeong0620@gmail.com>
2024-01-18 19:00:01 +0900

    testcode of other examples of calculate method

--- src/main/java/com/example/sample/SampleApplication.java
+++ src/main/java/com/example/sample/SampleApplication.java
@@ -2,7 +2,7 @@ package com.example.sample;
 
 import java.util.Scanner;
 
-public class Main {
+public class SampleApplication {
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
