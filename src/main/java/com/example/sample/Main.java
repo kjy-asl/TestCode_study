bd742e34516b9d904527b68798d4035669968b82
김주영 <gimjuyeong0620@gmail.com>
2024-01-18 18:04:18 +0900

    caculator

--- src/main/java/com/example/sample/Main.java
+++ src/main/java/com/example/sample/Main.java
@@ -6,10 +6,30 @@ public class Main {
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two numbers and an operator (e.g 1 + 2): ");
         String result = scanner.nextLine();
         String[] parts = result.split(" ");
-        for (String part : parts) {
-            System.out.println(part);
+
+        long num1 = Long.parseLong(parts[0]);
+        long num2 = Long.parseLong(parts[2]);
+        String operator = parts[1];
+        long answer = 0;
+
+        switch(operator){
+            case "+":
+                answer = num1 + num2;
+                break;
+            case "-":
+                answer = num1 - num2;
+                break;
+            case "*":
+                answer = num1 * num2;
+                break;
+            case "/":
+                answer = num1 / num2;
+            default:
+                throw new InvalidOperatorException();
         }
+        System.out.println(answer);
     }
 }
